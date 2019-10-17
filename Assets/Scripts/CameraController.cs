b54bca84983a8b2b8f3e65c6efc699b6251f4931
Guang <quocguangha@gmail.com>
2019-10-17 23:49:31 +0200

    initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private SoPlayerPosition playerPosition;
+
+    [SerializeField]
+    private SoCameraOffset cameraOffset;
+
+    private void Start()
+    {
+        //transform.position = playerPosition.position + cameraOffset.position;
+        //transform.rotation = Quaternion.Euler(cameraOffset.rotation);
+    }
+
+    void LateUpdate()
+    {
+        transform.position = playerPosition.position + cameraOffset.position;
+        transform.rotation = Quaternion.Euler(cameraOffset.rotation);
+        //transform.LookAt(playerPosition.player);
+    }
+}
---
+++ Assets/Scripts/SoPlayerPosition.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Player Position", menuName = "My Objects/Player Position", order = 1)]
+public class SoPlayerPosition : SoPosition
+{
+    
+}
