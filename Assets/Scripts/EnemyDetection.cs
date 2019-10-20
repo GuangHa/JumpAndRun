f5b61a0b34aa040a1784a27ea062d35731906cb9
Daniel Ehrler <daniel.ehrler@stud.hslu.ch>
2019-10-20 18:26:47 +0200

    new Tower and Bullet with scripts

---
+++ Assets/Scripts/EnemyDetection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDetection : MonoBehaviour
+{
+    public event Action EnemyDetected = delegate { };
+    [SerializeField]
+    private float range;
+    [SerializeField]
+    private Transform player;
+
+    private void Update()
+    {
+        if(Vector3.Distance(player.position, transform.position) <= range)
+        {
+            EnemyDetected();
+        }
+    } 
+        
+}
---
+++ Assets/Scripts/FireBullet.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBullet : MonoBehaviour
+{
+    private EnemyDetection ed;
+
+    [SerializeField]
+    private Bullet bullet;
+
+    [SerializeField]
+
+    private Transform bulletSpawnTransform;
+
+    [SerializeField]
+    private Transform bulletParent;
+
+    [SerializeField]
+    private float fireRate;
+
+    [SerializeField]
+    private Transform player;
+
+    private float nextFire;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ed = GetComponent<EnemyDetection>();
+        ed.EnemyDetected += Shoot;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Shoot()
+    {
+
+        if(Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+
+            bulletSpawnTransform.LookAt(player.position);
+
+            Instantiate(
+            bullet,
+            bulletSpawnTransform.position,
+            bulletSpawnTransform.rotation,
+            bulletParent.transform);
+        }
+        
+    }
+}
