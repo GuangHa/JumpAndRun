2784fe6552a567fdaf7c4b5928d71b1f9bc53eff
Suriya Suriyakanthan <suriya.suriyakanthan@stud.hslu.ch>
2019-10-31 17:53:57 +0100

    added menu

---
+++ Assets/HauptMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HauptMenu : MonoBehaviour
+{
+    // Beim Klicken von PlayButton
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level 1");
+        Debug.Log("Das Spiel wird gestartet!");
+   
+
+    }
+
+    public void SpielVerlassen()
+    {
+        Debug.Log("Das Spiel wird jetzt beendet!");
+        Application.Quit();
+    }
+   
+}
