c01cf49de706f61573c921e1ff3e15235928fc1e
Guang <quocguangha@gmail.com>
2019-12-07 17:40:44 +0100

    Add trampoline, fix volume changer, remodeled some area in playground

---
+++ Assets/Scripts/Essentials/Bounce.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour
+{
+    public float bounceAmount = 35;
+
+    private GameObject player;
+    private Rigidbody rbPlayer;
+    private bool bounce = false;
+    private GameObject soundManagerObject;
+    private SoundManager soundManager;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        rbPlayer = player.GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        soundManagerObject = GameObject.FindWithTag("SoundManager");
+        soundManager = soundManagerObject.GetComponent<SoundManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(bounce)
+        {
+            soundManager.audioSources[10].Play();
+            rbPlayer.AddForce(0, bounceAmount, 0, ForceMode.Impulse);
+            bounce = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.transform.root.gameObject == player)
+        {
+            bounce = true;
+        }
+    }
+}
