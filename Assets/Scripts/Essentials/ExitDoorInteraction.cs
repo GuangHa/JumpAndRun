978c65aa420b7cc1de187b31e7f844c87bce7238
Guang <quocguangha@gmail.com>
2019-12-07 22:00:33 +0100

    Add text for interaction

--- Assets/Scripts/Essentials/ExitDoorInteraction.cs
+++ Assets/Scripts/Essentials/ExitDoorInteraction.cs
@@ -6,17 +6,28 @@ public class ExitDoorInteraction : MonoBehaviour
 {
     private GameObject player;
     private GameObject soundManagerObject;
+    private GameObject exitInteractHUDObject;
     private SoundManager soundManager;
 
     private void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        exitInteractHUDObject = GameObject.FindGameObjectWithTag("ExitInteraction");
     }
 
     private void Start()
     {
         soundManagerObject = GameObject.FindWithTag("SoundManager");
         soundManager = soundManagerObject.GetComponent<SoundManager>();
+        exitInteractHUDObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.transform.root.gameObject == player)
+        {
+            exitInteractHUDObject.SetActive(true);
+        }
     }
 
     private void OnTriggerStay(Collider other)
@@ -26,5 +37,10 @@ public class ExitDoorInteraction : MonoBehaviour
             // play sound fbi_open_up
             soundManager.audioSources[9].Play();
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        exitInteractHUDObject.SetActive(false);
     }
 }
